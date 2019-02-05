7e35e2dcea6531f57aa5374494d81b2f7ecfa638
Carlos Requena López <crequena@ulb.ac.be>
2019-02-05 21:25:07 +0100

    fix #3, add fixed table

--- components/App.jsx
+++ components/App.jsx
@@ -3,15 +3,20 @@ import FilePicker from './FilePicker.jsx';
 import DataTable from './DataTable.jsx';
 import Store from './Store.js';
 import { Provider } from 'mobx-react';
+import styled from 'styled-components';
 
 const store = new Store();
+
+const SDataTable = styled(DataTable)`
+  margin: 1em;
+`;
 
 export default function App(props) {
   return (
     <Provider store={store}>
        <div>
           <FilePicker />
-          <DataTable />
+          <SDataTable />
        </div>
     </Provider>
   );
--- components/DataTable.jsx
+++ components/DataTable.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import Store from './Store';
-import { observer, inject } from "mobx-react"
+import { observer, inject } from 'mobx-react';
+import { Table, Column, Cell } from 'fixed-data-table-2';
 
 @inject("store")
 @observer
 export default class DataTable extends React.Component {
   render () {
     let rows = this.props.store.rows;
-    const listItems = rows.map((row, index) =>
-      <li key={index}>{row.high}</li>
+    let headers = this.props.store.headers;
+    const columns = headers.map((e) =>
+      <Column
+        key={e}
+        header={<Cell>{e}</Cell>}
+        columnKey={e}
+        cell={({ rowIndex, columnKey, ...props }) =>
+          <Cell {...props}>
+             {rows[rowIndex][columnKey]}
+          </Cell>}
+        width={200}
+      />
     );
+
     return (
-      <ul>
-         {listItems}
-      </ul>
+      <Table className={this.props.className}
+        rowHeight={50}
+        rowsCount={rows.length}
+        width={1300}
+        height={1000}
+        headerHeight={80}
+      >
+         {columns}
+      </Table>
     );
   }
 }
