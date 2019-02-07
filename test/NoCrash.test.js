afeb15c4e4721850530def659c75a25466559766
Carlos Requena López <crequena@ulb.ac.be>
2019-02-07 11:31:50 +0100

    mock worker so jest can run

--- test/NoCrash.test.js
+++ test/NoCrash.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../components/App';
+import App from '../components/App.jsx';
 
 it('renders without crashing', () => {
   shallow(<App />);
